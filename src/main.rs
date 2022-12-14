abcbddacde3cc32c5533a1122e1ba8ee49671143
sato <ysudqfs@163.com>
2022-12-14 16:27:27 +0800

    record

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,152 @@
+use std::{thread, time};
 use rust_scrcpy_client::ui::constants;
 use chrono::Local;
 
-use iced::{Alignment, button, Button, Column, Element, Length, Row, Sandbox, Settings, Text, container};
-use iced::alignment::Horizontal;
+use iced::theme::{self, Theme};
+use iced::widget::{button, column, container, row, text, horizontal_space, checkbox};
 
+use iced::{alignment, Alignment, executor, Element, keyboard, Length, Sandbox, Settings, Subscription, window, Application, Command};
+use iced::alignment::{Horizontal, Vertical};
+use iced::application::StyleSheet;
+
+use iced_native::{event, subscription, Event, Renderer};
 
 fn main() -> iced::Result {
     // run forever
-    Scrcpy::run(Settings::default())
+    Scrcpy::run(Settings {
+        window: window::Settings {
+            size: (constants::WIDTH as u32, constants::HEIGHT as u32),
+            ..window::Settings::default()
+        },
+        ..Settings::default()
+    })
 }
+
 
 struct Scrcpy {
-    value: i32,
     current_time: String,
-    // The local state of the two buttons
-    increment_button: button::State,
-    decrement_button: button::State,
+    check_all: bool
+
 }
 
+// event signal
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    IncrementPressed,
-    DecrementPressed,
+    UpdateTime,
+    UpdateTable,
+    Tick(time::Instant),
+    StartALl,
+    StopAll,
+    CheckAll(bool),
+    Run,
+    Edit,
+    Show,
 }
 
-impl Sandbox for Scrcpy {
+
+
+impl Application for Scrcpy {
+    type Executor = executor::Default;
     type Message = Message;
-    fn new() -> Self {
-        let fmt = "%Y-%m-%d %H:%M:%S";
-        let t = Local::now().format(fmt).to_string();
-        Self { value: 0, current_time: t, increment_button: Default::default(), decrement_button: Default::default() }
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            Scrcpy {
+                current_time: "".to_string(),
+                check_all: false,
+            },
+            Command::none(),
+        )
     }
+
     // 标题栏
     fn title(&self) -> String {
         String::from(constants::APP_NAME)
     }
 
     // 视图刷新
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            Message::IncrementPressed => {
-                self.value += 1;
+            Message::UpdateTime => {
+                let fmt = "%Y-%m-%d %H:%M:%S";
+                self.current_time = Local::now().format(fmt).to_string();
             }
-            Message::DecrementPressed => {
-                self.value -= 1;
+            Message::UpdateTable => {
+
+            }
+            Message::Tick(now) => {
+
+            }
+            _ => {
+
             }
         }
+        Command::none()
     }
-    // 布局
-    fn view(&mut self) -> Element<Message> {
-        Column::new().push(
-            Text::new(&self.current_time).horizontal_alignment(Horizontal::Left)
-        ).push(
-            Row::new().push(
-                Button::new(&mut self.increment_button, Text::new("+"))
-                    .on_press(Message::IncrementPressed).width(Length::Fill),
-            ).push(
-                Text::new(self.value.to_string()).size(22),
-            ).push(
-                Button::new(&mut self.decrement_button, Text::new("-"))
-                    .on_press(Message::DecrementPressed).width(Length::Fill),
-            )
-        ).into()
-        // .align_items(Alignment::Center).into()
+
+    fn view(&self) -> Element<Message>  {
+        let fmt = "%Y-%m-%d %H:%M:%S";
+        let current_time = text( Local::now().format(fmt).to_string())
+            .size(20).horizontal_alignment(Horizontal::Left);
+
+        let table_headers = row![text("id"), text("check"), text("SerialNum"), text("NickName"), text("RunMode"), text("Run"), text("Operate"), text("Other")].spacing(50).align_items(Alignment::Start).width(Length::Fill);
+
+        let check_box = checkbox(constants::CHECK_ALL, self.check_all, Message::CheckAll);
+
+        let all_start_btn = button(constants::ALL_START)
+            .style(theme::Button::Destructive)
+            .on_press(Message::StartALl);
+
+        let all_stop_btn = button(constants::ALL_STOP)
+            .style(theme::Button::Destructive)
+            .on_press(Message::StopAll);
+
+
+        let controls = row![check_box, all_start_btn, all_stop_btn].spacing(20).align_items(Alignment::End).height(Length::Fill);
+
+        let content = column![current_time, table_headers, controls]
+            .spacing(20);
+        // let content = container(
+        //     column![
+        //         row![
+        //             text("Top Left"),
+        //             horizontal_space(Length::Fill),
+        //             text("Top Right")
+        //         ]
+        //         .align_items(Alignment::Start)
+        //         .height(Length::Fill),
+        //         container(
+        //             button(text("Show Modal")).on_press(Message::StopAll)
+        //         )
+        //         .center_x()
+        //         .center_y()
+        //         .width(Length::Fill)
+        //         .height(Length::Fill),
+        //         row![
+        //             text("Bottom Left"),
+        //             horizontal_space(Length::Fill),
+        //             text("Bottom Right")
+        //         ]
+        //         .align_items(Alignment::End)
+        //         .height(Length::Fill),
+        //     ]
+        //         .height(Length::Fill),
+        // )
+        //     .padding(10)
+        //     .width(Length::Fill)
+        //     .height(Length::Fill);
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .align_x(Horizontal::Left)
+            .into()
     }
+
+    fn subscription(&self) -> Subscription<Message> {
+        Subscription::none()
+    }
+
 }
